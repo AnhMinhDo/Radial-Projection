d0b8e2536163103c923f8330738e8388a8755ee6
anhminh <ahd.minh@gmail.com>
2025-09-10 12:00:50 +0200

    refactor: only save the result to the model, when worker finishes, caller can get the result directly from model.

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/RadialProjectionWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/RadialProjectionWorker.java
@@ -5,22 +5,21 @@ import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
 import schneiderlab.tools.radialprojection.imageprocessor.core.polarprojection.PolarProjection;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 
-public class PolarProjectionWorker extends SwingWorker<Void, Void> {
+public class RadialProjectionWorker extends SwingWorker<Void, Void> {
     private final ImagePlus hybridStack;
     private final ImagePlus celluloseStack;
     private final ImagePlus ligninStack;
     private final ImagePlus edgeBinaryMaskEdge;
     private final ArrayList<Vessel> vesselArrayList;
-    private ArrayList<ImagePlus> vesselPolarProjectionArrayList;
+//    private ArrayList<ImagePlus> vesselPolarProjectionArrayList;
 
-    public PolarProjectionWorker(ImagePlus hybridStack,
-                                 ImagePlus celluloseStack,
-                                 ImagePlus ligninStack,
-                                 ImagePlus edgeBinaryMaskEdge,
-                                 ArrayList<Vessel> vesselArrayList) {
+    public RadialProjectionWorker(ImagePlus hybridStack,
+                                  ImagePlus celluloseStack,
+                                  ImagePlus ligninStack,
+                                  ImagePlus edgeBinaryMaskEdge,
+                                  ArrayList<Vessel> vesselArrayList) {
         this.hybridStack = hybridStack;
         this.celluloseStack = celluloseStack;
         this.ligninStack = ligninStack;
@@ -28,13 +27,13 @@ public class PolarProjectionWorker extends SwingWorker<Void, Void> {
         this.vesselArrayList= vesselArrayList;
     }
 
-    public ArrayList<ImagePlus> getVesselPolarProjectionArrayList() {
-        return vesselPolarProjectionArrayList;
-    }
+//    public ArrayList<ImagePlus> getVesselPolarProjectionArrayList() {
+//        return vesselPolarProjectionArrayList;
+//    }
 
     @Override
     protected Void doInBackground() {
-        vesselPolarProjectionArrayList = new ArrayList<>(vesselArrayList.size()*3);// 3 for lignin channel, cellulose channel, hybrid
+//        vesselPolarProjectionArrayList = new ArrayList<>(vesselArrayList.size()*3);// 3 for lignin channel, cellulose channel, hybrid
         int currentProgress = 0;
         int increment = (int) 100.0/(vesselArrayList.size()*3);
         setProgress(currentProgress);
@@ -69,12 +68,12 @@ public class PolarProjectionWorker extends SwingWorker<Void, Void> {
             vesselPolarProjectionHybrid.setTitle(imageTitleHybrid);
             vesselPolarProjectionCellulose.setTitle(imageTitleCellulose);
             vesselPolarProjectionLignin.setTitle(imageTitleLignin);
-            vesselArrayList.get(i).setRadialProjectionHybrid(vesselPolarProjectionHybrid.duplicate());
-            vesselArrayList.get(i).setRadialProjectionLignin(vesselPolarProjectionLignin.duplicate());
-            vesselArrayList.get(i).setRadialProjectionCellulose(vesselPolarProjectionCellulose.duplicate());
-            vesselPolarProjectionArrayList.add(vesselPolarProjectionHybrid);
-            vesselPolarProjectionArrayList.add(vesselPolarProjectionCellulose);
-            vesselPolarProjectionArrayList.add(vesselPolarProjectionLignin);
+            vesselArrayList.get(i).setRadialProjectionHybrid(vesselPolarProjectionHybrid);
+            vesselArrayList.get(i).setRadialProjectionLignin(vesselPolarProjectionLignin);
+            vesselArrayList.get(i).setRadialProjectionCellulose(vesselPolarProjectionCellulose);
+//            vesselPolarProjectionArrayList.add(vesselPolarProjectionHybrid);
+//            vesselPolarProjectionArrayList.add(vesselPolarProjectionCellulose);
+//            vesselPolarProjectionArrayList.add(vesselPolarProjectionLignin);
 
         }
         return null;
