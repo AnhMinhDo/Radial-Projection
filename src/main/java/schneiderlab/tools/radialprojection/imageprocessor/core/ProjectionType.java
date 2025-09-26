596564540c1faefe8b12f231236ad26e2d660828
anhminh <ahd.minh@gmail.com>
2025-09-26 23:36:07 +0200

    feat: enum for the type of Radial Projection or Unroll

---
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/ProjectionType.java
@@ -0,0 +1,11 @@
+package schneiderlab.tools.radialprojection.imageprocessor.core;
+
+public enum ProjectionType {
+    RADIAL_LIGNIN,
+    RADIAL_CELLULOSE,
+    RADIAL_HYBRID,
+    UNROLL_LIGNIN,
+    UNROLL_CELLULOSE,
+    UNROLL_HYBRID
+
+}
