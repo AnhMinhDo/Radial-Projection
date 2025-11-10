d09f8e3b7b5df7d7a26143cef1dd3c8a5ea3e625
anhminh <ahd.minh@gmail.com>
2025-11-10 09:41:15 +0100

    feat: the RadialProjectionModel holds the objects of ImageWindow shown on the screen for radial projection and unrolling

--- src/main/java/schneiderlab/tools/radialprojection/models/radialprojection/RadialProjectionModel.java
+++ src/main/java/schneiderlab/tools/radialprojection/models/radialprojection/RadialProjectionModel.java
@@ -1,7 +1,12 @@
 package schneiderlab.tools.radialprojection.models.radialprojection;
 
 import ij.ImagePlus;
+import ij.gui.ImageWindow;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.FloatType;
+import schneiderlab.tools.radialprojection.imageprocessor.core.ImageData;
 import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
+import schneiderlab.tools.radialprojection.views.userinterfacecomponents.ImageWindowGroupController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +16,19 @@ public class RadialProjectionModel {
     private ImagePlus hybrid;
     private ImagePlus cellulose;
     private List<Vessel> vesselArrayList;
+    private ImageData<UnsignedShortType, FloatType> imageData;
+    private List<ImageWindowGroupController> vesselRadialProjectionImageWindowGroupList = new ArrayList<>();
+    private List<ImageWindowGroupController> vesselUnrollImageWindowGroupList = new ArrayList<>();
 
     public RadialProjectionModel() {
+    }
+
+    public ImageData<UnsignedShortType, FloatType> getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(ImageData<UnsignedShortType, FloatType> imageData) {
+        this.imageData = imageData;
     }
 
     public ImagePlus getLignin() {
@@ -46,4 +62,30 @@ public class RadialProjectionModel {
     public void setVesselArrayList(List<Vessel> vesselArrayList) {
         this.vesselArrayList = vesselArrayList;
     }
+
+    public List<ImageWindowGroupController> getVesselRadialProjectionImageWindowGroupList() {
+        return vesselRadialProjectionImageWindowGroupList;
+    }
+
+    public void addVesselRadialProjectionImageWindowGroup(ImageWindowGroupController vesselRadialProjectionImageWindowGroup) {
+        this.vesselRadialProjectionImageWindowGroupList.add(vesselRadialProjectionImageWindowGroup);
+    }
+
+    public void closeAllImageWindowGroup(){
+        for(ImageWindowGroupController imageWindowGroupController: vesselRadialProjectionImageWindowGroupList){
+            imageWindowGroupController.closeAllWindowInGroup();
+        }
+        for (ImageWindowGroupController imageWindowGroupController: vesselUnrollImageWindowGroupList){
+            imageWindowGroupController.closeAllWindowInGroup();
+        }
+    }
+
+    public List<ImageWindowGroupController> getVesselUnrollImageWindowGroupList() {
+        return vesselUnrollImageWindowGroupList;
+    }
+
+    public void addVesselUnrollImageWindowGroup(ImageWindowGroupController vesselUnrollImageWindowGroup) {
+        this.vesselUnrollImageWindowGroupList.add(vesselUnrollImageWindowGroup) ;
+    }
+
 }
