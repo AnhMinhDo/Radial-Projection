338c9e9f164e8373e0f5ddd4a10733ddb9aff765
anhminh <ahd.minh@gmail.com>
2025-11-10 09:42:31 +0100

    feat: add flipping horizontally feature for radial projection worker

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/RadialProjectionWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/RadialProjectionWorker.java
@@ -1,30 +1,42 @@
 package schneiderlab.tools.radialprojection.controllers.workers;
 
 import ij.ImagePlus;
+import org.scijava.Context;
 import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
 import schneiderlab.tools.radialprojection.imageprocessor.core.polarprojection.PolarProjection;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RadialProjectionWorker extends SwingWorker<Void, Void> {
-    private final ImagePlus hybridStack;
+    private final ImagePlus hybridRawStack;
+    private final ImagePlus hybridSmoothedStack;
     private final ImagePlus celluloseStack;
     private final ImagePlus ligninStack;
     private final ImagePlus edgeBinaryMaskEdge;
     private final List<Vessel> vesselArrayList;
+    private double xyPixelSizeInMicron;
+    private boolean isFlipHorizontally;
+    private Context context;
 
-    public RadialProjectionWorker(ImagePlus hybridStack,
+    public RadialProjectionWorker(ImagePlus hybridRawStack,
+                                  ImagePlus hybridSmoothedStack,
                                   ImagePlus celluloseStack,
                                   ImagePlus ligninStack,
                                   ImagePlus edgeBinaryMaskEdge,
-                                  List<Vessel> vesselArrayList) {
-        this.hybridStack = hybridStack;
+                                  List<Vessel> vesselArrayList,
+                                  double xyPixelSizeInMicron,
+                                  boolean isFlipHorizontally,
+                                  Context context) {
+        this.hybridRawStack = hybridRawStack;
+        this.hybridSmoothedStack = hybridSmoothedStack;
         this.celluloseStack = celluloseStack;
         this.ligninStack = ligninStack;
         this.edgeBinaryMaskEdge =edgeBinaryMaskEdge;
         this.vesselArrayList= vesselArrayList;
+        this.xyPixelSizeInMicron = xyPixelSizeInMicron;
+        this.isFlipHorizontally = isFlipHorizontally;
+        this.context= context;
     }
 
 //    public ArrayList<ImagePlus> getVesselPolarProjectionArrayList() {
@@ -38,21 +50,26 @@ public class RadialProjectionWorker extends SwingWorker<Void, Void> {
         int increment = (int) 100.0/(vesselArrayList.size()*3);
         setProgress(currentProgress);
         for (int i = 0; i < vesselArrayList.size(); i++) { // for each vessel
-            PolarProjection polarProjectionHybrid = new PolarProjection(hybridStack,
+            PolarProjection polarProjectionHybrid = new PolarProjection(hybridSmoothedStack,
+                    hybridRawStack,
                     edgeBinaryMaskEdge,
-                    vesselArrayList.get(i).getCentroidArrayList(),
-                    5 // 5 degree is considered adequately small angle
-            );
-            PolarProjection polarProjectionCellulose = new PolarProjection(celluloseStack,
+                    vesselArrayList.get(i),
+                    5, // 5 degree is considered adequately small angle
+            xyPixelSizeInMicron,
+                    context);
+            PolarProjection polarProjectionCellulose = new PolarProjection(hybridSmoothedStack,celluloseStack,
                     edgeBinaryMaskEdge,
-                    vesselArrayList.get(i).getCentroidArrayList(),
-                    5 // 5 degree is considered adequately small angle
-            );
-            PolarProjection polarProjectionLignin = new PolarProjection(ligninStack,
+                    vesselArrayList.get(i),
+                    5, // 5 degree is considered adequately small angle
+                    xyPixelSizeInMicron,
+                    context);
+            PolarProjection polarProjectionLignin = new PolarProjection(hybridSmoothedStack,
+                    ligninStack,
                     edgeBinaryMaskEdge,
-                    vesselArrayList.get(i).getCentroidArrayList(),
-                    5 // 5 degree is considered adequately small angle
-            );
+                    vesselArrayList.get(i),
+                    5, // 5 degree is considered adequately small angle
+                    xyPixelSizeInMicron,
+                    context);
             ImagePlus vesselPolarProjectionHybrid=polarProjectionHybrid.process();
             currentProgress= currentProgress+increment;
             setProgress(currentProgress);
@@ -68,13 +85,14 @@ public class RadialProjectionWorker extends SwingWorker<Void, Void> {
             vesselPolarProjectionHybrid.setTitle(imageTitleHybrid);
             vesselPolarProjectionCellulose.setTitle(imageTitleCellulose);
             vesselPolarProjectionLignin.setTitle(imageTitleLignin);
+            if(isFlipHorizontally){
+                vesselPolarProjectionHybrid.getProcessor().flipHorizontal();
+                vesselPolarProjectionLignin.getProcessor().flipHorizontal();
+                vesselPolarProjectionCellulose.getProcessor().flipHorizontal();
+            }
             vesselArrayList.get(i).setRadialProjectionHybrid(vesselPolarProjectionHybrid);
             vesselArrayList.get(i).setRadialProjectionLignin(vesselPolarProjectionLignin);
             vesselArrayList.get(i).setRadialProjectionCellulose(vesselPolarProjectionCellulose);
-//            vesselPolarProjectionArrayList.add(vesselPolarProjectionHybrid);
-//            vesselPolarProjectionArrayList.add(vesselPolarProjectionCellulose);
-//            vesselPolarProjectionArrayList.add(vesselPolarProjectionLignin);
-
         }
         return null;
     }
