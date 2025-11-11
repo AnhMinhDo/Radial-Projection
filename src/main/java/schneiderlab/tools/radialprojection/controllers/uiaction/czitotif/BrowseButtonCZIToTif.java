fb9a3309ba52b0b42635471b0230930ca2dd9994
anhminh <ahd.minh@gmail.com>
2025-11-11 17:17:47 +0100

    feat : file the file Name table with file name from the selected directory

--- src/main/java/schneiderlab/tools/radialprojection/controllers/uiaction/czitotif/BrowseButtonCZIToTif.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/uiaction/czitotif/BrowseButtonCZIToTif.java
@@ -1,16 +1,22 @@
 package schneiderlab.tools.radialprojection.controllers.uiaction.czitotif;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class BrowseButtonCZIToTif implements ActionListener {
     private JTextField textFieldFolderPath;
     private JFrame parentFrame;
+    private JTable table;
 
-    public BrowseButtonCZIToTif(JTextField textFieldFolderPath, JFrame parentFrame) {
+    public BrowseButtonCZIToTif(JTextField textFieldFolderPath,
+                                JFrame parentFrame,
+                                JTable table) {
         this.textFieldFolderPath = textFieldFolderPath;
         this.parentFrame = parentFrame;
+        this.table = table;
     }
 
     @Override
@@ -19,8 +25,18 @@ public class BrowseButtonCZIToTif implements ActionListener {
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int returnVal = chooser.showOpenDialog(parentFrame);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
-            textFieldFolderPath.setText(chooser.getSelectedFile().getAbsolutePath());
-    }
+            File selectedDir = chooser.getSelectedFile();
+            textFieldFolderPath.setText(selectedDir.getAbsolutePath());
+            File[] filesList = selectedDir.listFiles(((dir, name) -> name.toLowerCase().endsWith(".czi")));
+            if(filesList==null){
+                return;
+            }
+            DefaultTableModel cziToTifTableModel = (DefaultTableModel) table.getModel();
+            cziToTifTableModel.setRowCount(0);
+            for (int i = 0; i < filesList.length; i++) {
+                cziToTifTableModel.addRow(new String[]{filesList[i].getName()});
+            }
+        }
 }
 
 }
