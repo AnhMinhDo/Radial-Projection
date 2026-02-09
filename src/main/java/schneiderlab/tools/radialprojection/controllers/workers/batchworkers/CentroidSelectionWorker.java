e52ce40c9fac790866162bf607c995e55276a49e
anhminh <ahd.minh@gmail.com>
2026-02-09 10:38:52 +0100

    feat: centroid selection class receives the path of the ser file and use this file to retrieve the image info instead of depending on the global ViewModel, at the end, new ser file is created and saved to storage

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/batchworkers/CentroidSelectionWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/batchworkers/CentroidSelectionWorker.java
@@ -1,34 +1,53 @@
 package schneiderlab.tools.radialprojection.controllers.workers.batchworkers;
 
+import ij.IJ;
 import ij.ImagePlus;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 import org.scijava.Context;
 import schneiderlab.tools.radialprojection.imageprocessor.core.ImageData;
+import schneiderlab.tools.radialprojection.imageprocessor.core.imagedataserialized.ImageDataSerializable;
+import schneiderlab.tools.radialprojection.imageprocessor.core.imagedataserialized.ImageDataSerializableUtils;
+import schneiderlab.tools.radialprojection.models.batch.BatchModeGlobalStateModel;
 import schneiderlab.tools.radialprojection.models.batch.BatchModeModel;
 import schneiderlab.tools.radialprojection.views.userinterfacecomponents.ImageWindowCentroidSelection;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.nio.file.Paths;
 import java.util.concurrent.CountDownLatch;
 
 public class CentroidSelectionWorker extends SwingWorker<Void, Void> {
     private BatchModeModel batchModeModel;
+    private BatchModeGlobalStateModel batchModeGlobalStateModel;
     private Context context;
-    public CentroidSelectionWorker(BatchModeModel batchModeModel, Context context) {
+
+    public CentroidSelectionWorker(BatchModeModel batchModeModel, BatchModeGlobalStateModel batchModeGlobalStateModel, Context context) {
         this.batchModeModel = batchModeModel;
+        this.batchModeGlobalStateModel = batchModeGlobalStateModel;
+        this.context = context;
     }
 
     @Override
     protected Void doInBackground() throws Exception {
+        IJ.log("start the centroid selection worker");
         int totalTaskNumber = batchModeModel.getCentroidelectionList().size();
-        for(ImageData imageData: batchModeModel.getCentroidelectionList()){
+        while(!batchModeGlobalStateModel.getCentroidSelectionQueue().isEmpty())
+            {
+                String serFile = batchModeGlobalStateModel.getFirstCentroidSelectionQueue();
+                IJ.log("serialized object path: " + serFile);
+                ImageDataSerializable imageDataSerializable = ImageDataSerializableUtils.imageDataDeserializeObject(Paths.get(serFile));
+                IJ.log("complete deserialization");
+                ImageData<UnsignedShortType, FloatType> imageData = ImageDataSerializableUtils.converSerializableToImageData(imageDataSerializable, context);
+                IJ.log("complete conversion to ImageData object");
+//            imageData = imageDataRetrieved;
             RandomAccessibleInterval<FloatType> smoothedStack  = imageData.getHybridStackSmoothed();
             int slideForTuning = 0;
             // get the firstSlide
@@ -55,7 +74,11 @@ public class CentroidSelectionWorker extends SwingWorker<Void, Void> {
             });
             latch.await();
             setProgress(totalTaskNumber-=1);
-            batchModeModel.addImageDataToWatershedList(imageData);
+            batchModeGlobalStateModel.addLastWatershedRadialProjectionQueue(serFile);
+            batchModeGlobalStateModel.removeFirstCentroidSelectionQueue();
+            // serialize the imageData object to ser file, include the centroid coordinates
+                imageDataSerializable.setUserSelectedCentroidsList(imageData.getUserSelectedCentroidsList());
+                imageDataSerializable.serializeObject();
         }
         return null;
     }
