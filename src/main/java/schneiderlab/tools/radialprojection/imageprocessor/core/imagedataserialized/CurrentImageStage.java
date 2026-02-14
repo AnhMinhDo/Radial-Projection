0cf851cbdd6793ea5c76901931ee7b7c03cdf7ec
anhminh <ahd.minh@gmail.com>
2026-02-14 10:45:31 +0100

    enum class to inform the stage of the application

---
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/imagedataserialized/CurrentImageStage.java
@@ -0,0 +1,9 @@
+package schneiderlab.tools.radialprojection.imageprocessor.core.imagedataserialized;
+
+public enum CurrentImageStage {
+    Start,
+    CentroidSelection,
+    WatershedAndRadialProjection,
+    RefineVessel,
+    Analysis
+}
