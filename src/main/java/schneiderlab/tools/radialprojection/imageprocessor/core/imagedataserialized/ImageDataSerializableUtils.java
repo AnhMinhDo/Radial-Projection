8a838dfe61481b847a0cfa65e5451aec9aba4ec0
anhminh <ahd.minh@gmail.com>
2026-02-26 17:02:28 +0100

    feature: add instruction at Refinement step to handle the conversion between imageData object and ImageDataSerializable object

--- src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/imagedataserialized/ImageDataSerializableUtils.java
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/imagedataserialized/ImageDataSerializableUtils.java
@@ -12,6 +12,9 @@ import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 import org.scijava.Context;
 import schneiderlab.tools.radialprojection.imageprocessor.core.ImageData;
+import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
+import schneiderlab.tools.radialprojection.imageprocessor.core.vesselserialized.VesselSerializable;
+import schneiderlab.tools.radialprojection.imageprocessor.core.vesselserialized.VesselSerializableUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -25,34 +28,61 @@ public class ImageDataSerializableUtils {
         ImageDataSerializable imageDataSerializable = new ImageDataSerializable();
         // path to save the ser file
         Path serPath = imageData.getSerializedObjectPath();
+
         imageDataSerializable.setSerializedObjectPath(serPath.toAbsolutePath().toString());
+
 //        IJ.log("Path of .ser file: " + serPath);
         // set the information from the ImageData to the ImageDataSerializable
         imageDataSerializable.setImagePath(imageData.getImagePath().toAbsolutePath().toString());
         imageDataSerializable.setOutputDirPath(imageData.getImageOutputPath().toAbsolutePath().toString());
+
         imageDataSerializable.setTempDir(imageData.getTempDirPath().toAbsolutePath().toString());
+
         imageDataSerializable.setHybridStackSmoothedWidth(imageData.getHybridStackSmoothedWidth());
+
         imageDataSerializable.setHybridStackSmoothedHeight(imageData.getHybridStackSmoothedHeight());
+
         imageDataSerializable.setXyPixelSize(imageData.getXyPixelSize());
+
         imageDataSerializable.setzPixelSize(imageData.getzPixelSize());
+
         imageDataSerializable.setAnalysisWindow(imageData.getAnalysisWindow());
+
+        imageDataSerializable.setRandomBoxWidth(imageData.getRandomBoxWidth());
+
+        imageDataSerializable.setNoOfRandomBox(imageData.getNoOfRandomBox());
+
+        imageDataSerializable.setLengthOfLineScan(imageData.getLengthOfLineScan());
+
+        imageDataSerializable.setNoOfRandomLineScan(imageData.getNoOfRandomLineScan());
+
         imageDataSerializable.setCelluloseToLigninRatio(imageData.getCelluloseToLigninRatio());
+
         imageDataSerializable.setInnerVesselRadius(imageData.getInnerVesselRadius());
+
         imageDataSerializable.setSmoothingSigma(imageData.getSmoothingSigma());
+
         imageDataSerializable.setSideViewTempPathWithoutEdgeCentroid(imageData.getSideViewTempPathWithoutEdgeCentroid().toString());
+
         imageDataSerializable.setUserSelectedCentroidsList(imageData.getUserSelectedCentroidsList());
-//        imageDataSerializable.setSideViewLigninPath(imageData.getSideViewLigninPath().toAbsolutePath().toString());
-//        imageDataSerializable.setSideViewCellulosePath(imageData.getSideViewCellulosePath().toAbsolutePath().toString());
-//        imageDataSerializable.setSideViewHybridPath(imageData.getSideViewHybridPath().toAbsolutePath().toString());
-//        imageDataSerializable.setSideViewHybridSmoothedPath(imageData.getSideViewHybridSmoothedPath().toAbsolutePath().toString());
+
+        // path to the vessel.ser files as a List<String>
+        for(Path vesselSerFilePath : imageData.getVesselSerFilePathList()){
+            imageDataSerializable.addPathToVesselSerFilePathList(vesselSerFilePath.toString());
+        }
+
+        // watershed and radial Projection info
+
         return imageDataSerializable;
     }
 
     public static ImageData<UnsignedShortType, FloatType>
-    converSerializableToImageData(ImageDataSerializable imageDataSerializable, CurrentImageStage currentImageStage , Context context) {
+    convertSerializableToImageData(ImageDataSerializable imageDataSerializable, CurrentImageStage currentImageStage , Context context) {
         ImageData<UnsignedShortType, FloatType> imageData = new ImageData<>();
         imageData.setImagePath(Paths.get(imageDataSerializable.getImagePath()));
-        imageData.setOutputDirPath(Paths.get(imageDataSerializable.getOutputDirPath()));
+        imageData.setSerializedObjectPath(Paths.get(imageDataSerializable.getSerializedObjectPath()));
+//        imageData.setOutputDirPath(Paths.get(imageDataSerializable.getOutputDirPath()));
+        imageData.setImageOutputPath(Paths.get(imageDataSerializable.getOutputDirPath()));
         imageData.setTempDirPath(Paths.get(imageDataSerializable.getTempDir()));
         imageData.setXyPixelSize(imageDataSerializable.getXyPixelSize());
         imageData.setzPixelSize(imageDataSerializable.getzPixelSize());
@@ -64,6 +94,10 @@ public class ImageDataSerializableUtils {
         imageData.setUserSelectedCentroidsList(imageDataSerializable.getUserSelectedCentroidsList());
         imageData.setHybridStackSmoothedHeight(imageDataSerializable.getHybridStackSmoothedHeight());
         imageData.setHybridStackSmoothedWidth(imageDataSerializable.getHybridStackSmoothedWidth());
+        imageData.setNoOfRandomBox(imageDataSerializable.getNoOfRandomBox());
+        imageData.setRandomBoxWidth(imageDataSerializable.getRandomBoxWidth());
+        imageData.setLengthOfLineScan(imageDataSerializable.getLengthOfLineScan());
+        imageData.setNoOfRandomLineScan(imageDataSerializable.getNoOfRandomLineScan());
 //        imageData.setSideViewLigninPath(Paths.get(imageDataSerializable.getSideViewLigninPath()));
 //        imageData.setSideViewCellulosePath(Paths.get(imageDataSerializable.getSideViewCellulosePath()));
 //        imageData.setSideViewHybridPath(Paths.get(imageDataSerializable.getSideViewHybridPath()));
@@ -91,15 +125,22 @@ public class ImageDataSerializableUtils {
                 imageData.setLignin(ligninRAI);
                 imageData.setCellulose(celluloseRAI);
             }
-
-//            RandomAccessibleInterval<FloatType> ligninRAI = ops.convert().float32((ImgPlus<FloatType>)lignin.getImgPlus());
-//            RandomAccessibleInterval<FloatType> celluloseRAI = ops.convert().float32((ImgPlus<FloatType>)cellulose.getImgPlus());
-//            RandomAccessibleInterval<FloatType> hybridRAI = ops.convert().float32((ImgPlus<FloatType>)hybrid.getImgPlus());
-//            RandomAccessibleInterval<FloatType> hybridSmoothedRAI = ops.convert().float32((ImgPlus<FloatType>)hybridSmooth.getImgPlus());
-//            imageData.setLignin(ligninRAI);
-//            imageData.setCellulose(celluloseRAI);
+            if(CurrentImageStage.RefineVessel.equals(currentImageStage)){
+                // reload the vessel ser file path list and perform vessel deserialization
+                for(String vesselSerFilePath : imageDataSerializable.getVesselSerFilePathList()){
+                    imageData.addPathToVesselSerFilePathList(Paths.get(vesselSerFilePath));
+                }
+                for (Path vesselSerFilePath : imageData.getVesselSerFilePathList()){
+                    // deserialize the vessel object
+                    VesselSerializable vesselSerializable = VesselSerializableUtils.vesselDeserializeObject(vesselSerFilePath);
+                    Vessel vessel = VesselSerializableUtils.convertSerializableToVessel(vesselSerializable,CurrentImageStage.RefineVessel,context);
+                    imageData.addToVesselList(vessel);
+                }
+            }
+            if(CurrentImageStage.Analysis.equals(currentImageStage)){
+                //TODO
+            }
             imageData.setHybridStackSmoothed(hybridSmoothedRAI);
-//            imageData.setHybridStackNonSmoothed(hybridRAI);
             IJ.log("complete ImageData object creation");
             return imageData;
         } catch (IOException e) {
