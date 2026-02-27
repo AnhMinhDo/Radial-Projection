0f40f78eb0ae921b03a18f570de7a249ba9e9abb
anhminh <ahd.minh@gmail.com>
2026-02-27 10:15:52 +0100

    feature: use custom StackWindow class to view the output and let the user select a new range

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/batchworkers/BatchRefineVesselWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/batchworkers/BatchRefineVesselWorker.java
@@ -1,67 +1,72 @@
 package schneiderlab.tools.radialprojection.controllers.workers.batchworkers;
 
+import ij.IJ;
 import ij.ImagePlus;
-import net.imglib2.type.numeric.integer.UnsignedShortType;
-import net.imglib2.type.numeric.real.FloatType;
 import org.scijava.Context;
-import schneiderlab.tools.radialprojection.imageprocessor.core.ImageData;
-import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
-import schneiderlab.tools.radialprojection.models.batch.BatchModeModel;
-import schneiderlab.tools.radialprojection.views.userinterfacecomponents.ImageWindowGroupController;
+import schneiderlab.tools.radialprojection.imageprocessor.core.imagedataserialized.ImageDataSerializable;
+import schneiderlab.tools.radialprojection.imageprocessor.core.imagedataserialized.ImageDataSerializableUtils;
+import schneiderlab.tools.radialprojection.imageprocessor.core.vesselserialized.VesselSerializable;
+import schneiderlab.tools.radialprojection.imageprocessor.core.vesselserialized.VesselSerializableUtils;
+import schneiderlab.tools.radialprojection.models.batch.BatchModeGlobalStateModel;
+import schneiderlab.tools.radialprojection.views.userinterfacecomponents.StackWindowRefineVesselMultipleChannelsImagePlus;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class BatchRefineVesselWorker extends SwingWorker<Void, Void> {
-    private List<ImageData<UnsignedShortType, FloatType>> imageDataList;
-    private BatchModeModel batchModeModel;
+//    private List<ImageData<UnsignedShortType, FloatType>> imageDataList;
+//    private BatchModeModel batchModeModel;
+    private BatchModeGlobalStateModel batchModeGlobalStateModel;
     private Context context;
     private int singleFileProgress = 0;
 
-    public BatchRefineVesselWorker(BatchModeModel batchModeModel) {
-        this.batchModeModel = batchModeModel;
-        this.imageDataList = this.batchModeModel.getRefineVesselList();
+    public BatchRefineVesselWorker(BatchModeGlobalStateModel batchModeGlobalStateModel, Context context) {
+        this.batchModeGlobalStateModel = batchModeGlobalStateModel;
+//        this.imageDataList = this.batchModeModel.getRefineVesselList();
+        this.context = context;
     }
 
     @Override
     protected Void doInBackground() throws Exception {
-        double total = imageDataList.size();
-        double imageDataIndex = 0;
-        for(ImageData<UnsignedShortType, FloatType> imageData : imageDataList){
-            for (int i = 0; i < imageData.getVesselList().size(); i++) {
-                Vessel vessel = imageData.getVesselList().get(i);
-                // view the Vessel image with imageWindowGroupController
+        IJ.log("Start the refinement step");
+        while(!batchModeGlobalStateModel.getRefineVesselQueue().isEmpty()){
+            // get the path to imageDataSerializable
+            String serFile = batchModeGlobalStateModel.getFirstRefineVesselQueue();
+            // load the temp ser file and deserialize, no need to convert to ImageData, get the list to vessel ser file path
+            IJ.log("serialized object path: " + serFile);
+            ImageDataSerializable imageDataSerializable = ImageDataSerializableUtils.imageDataDeserializeObject(Paths.get(serFile));
+            IJ.log("complete deserialization");
+            // load the vesselSerFileFilePathList, no need to convert back to Vessel object
+            List<String> vesselFilePathList = imageDataSerializable.getVesselSerFilePathList();
+            for (String vesselFilePath : vesselFilePathList){
+                // deserialize the vesselSerializable object
+                VesselSerializable vesselSerializable = VesselSerializableUtils.vesselDeserializeObject(Paths.get(vesselFilePath));
+                // get the path to Radial Projection
+                String pathMultiChannelsRadialProjection = vesselSerializable.getPathMultiChannelsRadialProjection();
+                // load the radial projection using the file path
+                ImagePlus radialProjectionMultipleChannels = IJ.openImage(pathMultiChannelsRadialProjection);
+                // show the radial projection using custom ImageWindow
                 int rectangleToolID = 0;
-                ImagePlus rpHybrid = vessel.getRadialProjectionHybrid();
-                ImagePlus rpLignin = vessel.getRadialProjectionLignin();
-                ImagePlus rpCellulose = vessel.getRadialProjectionCellulose();
-                ImagePlus urHybrid = vessel.getUnrolledVesselHybrid();
-                ImagePlus urLignin = vessel.getUnrolledVesselLignin();
-                ImagePlus urCellulose = vessel.getRadialProjectionCellulose();
-                List<ImagePlus> imagelist = Arrays.asList(rpHybrid,rpLignin,rpCellulose,urHybrid,urLignin,urCellulose);
-                ImageWindowGroupController iwgc = new ImageWindowGroupController(imagelist,vessel,rectangleToolID);
-                CountDownLatch countDownLatch = new CountDownLatch(1);
-                rpHybrid.getWindow().addWindowListener(new WindowAdapter() {
-                    @Override
+                CountDownLatch latch = new CountDownLatch(1);
+                StackWindowRefineVesselMultipleChannelsImagePlus iwrvmcip = new StackWindowRefineVesselMultipleChannelsImagePlus(radialProjectionMultipleChannels,vesselSerializable);
+                // using window adapter to listen to window close to countdown latch
+                iwrvmcip.addWindowListener(new WindowAdapter() {
                     public void windowClosed(WindowEvent e) {
-                        iwgc.closeAllWindowInGroup();
-                        countDownLatch.countDown();
+                        vesselSerializable.serializeObject();
+                        latch.countDown();
                     }
                 });
-                countDownLatch.await();
+                latch.await();
             }
-            batchModeModel.addImageDataToAnalysisBatchList(imageData);
-            batchModeModel.setNumberOfImageDataInRefineVesselStep(batchModeModel.getNumberOfImageDataInRefineVesselStep()-1);
-            setProgress((int)Math.floor((imageDataIndex / total)*100));
-            imageDataIndex+=1;
+            // push the file name to the next queue
+            batchModeGlobalStateModel.addLastAnalysisQueue(serFile);
+            // remove the file name from the current queue
+            batchModeGlobalStateModel.removeFirstRefineVesselQueue();
         }
-
         return null;
     }
 }
